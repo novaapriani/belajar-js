ccf81aa47b99aaf24cffa816c21a06aa203d8764
novaapriani <novaapriani531@gmail.com>
2022-10-27 17:32:00 +0700

    tambah variabel, tipe data, function

---
+++ script.js
@@ -0,0 +1,16 @@
+// variabel dan tipe data
+let nama = 'Nova Apriani'
+let umur = 17
+let hobi = ['rebahan', 'makan', 'repeat']
+let masihSekolah = true
+
+// aritmatika
+const phi = 3.14
+let jariJari = 12
+let hasil = phi * jariJari
+console.log(`Luas lingkaran = ${hasil}`)
+
+// function
+let hitungLuasSegitiga = (alas, tinggi) => alas * tinggi / 2
+let hasilLuasSegitiga = hitungLuasSegitiga(8, 4)
+console.log(`Luas segitiga = ${hasilLuasSegitiga}`)
